799d90697a6d4caa81be8658200eb6a30f42db98
Heisenberg <omaradelawad20013@gmail.com>
2024-02-09 11:38:31 +0200

    categories list

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,12 @@ import CategoriesList from "@/components/CategoriesList";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 gap-16">
-      <p className="col-span-2">
+    <div className="mx-72 grid grid-cols-7 gap-16">
+      <p className="col-span-5">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
         recusandae.
       </p>
-      <div className="col-span-1">
-        <h2 className="text-xl text-violet-600 font-semibold">Categories</h2>
-        <CategoriesList />
-      </div>
+      <CategoriesList />
     </div>
   );
 }
