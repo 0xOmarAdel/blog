042e8de488d2d52bc4b1ae22bca7c457b520fa98
Heisenberg <omaradelawad20013@gmail.com>
2024-02-09 10:02:56 +0200

    created navbar

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,5 +1,38 @@
+import Link from "next/link";
+import { AiOutlineSearch } from "react-icons/ai";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="bg-white py-5 px-72 flex flex-row justify-between items-center shadow-sm">
+      <Link href="/" className="text-4xl text-primary font-semibold">
+        Blog
+      </Link>
+      <ul className="flex flex-row gap-5 text-lg text-gray-500 font-medium">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">Top Articles</Link>
+        </li>
+        <li>
+          <Link href="/">All Articles</Link>
+        </li>
+        <li>
+          <Link href="/">Contact Us</Link>
+        </li>
+      </ul>
+      <div className="relative ">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-60 py-[5px] px-2 border border-gray-300 rounded-md"
+        />
+        <div className="absolute top-0 right-0 h-full p-3 bg-primary rounded-ee-md rounded-se-md flex flex-row items-center">
+          <AiOutlineSearch className="text-xl text-white" />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
