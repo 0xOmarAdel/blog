1b0746fa6cedb9795efd3c4431ec7e79928fc1ff
Heisenberg <omaradelawad20013@gmail.com>
2024-02-10 07:43:10 +0200

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import CategoriesList from "@/components/CategoriesList";
+import Articles from "@/components/Articles";
 
 export default function Home() {
-  return (
-    <div className="mx-72 grid grid-cols-7 gap-16">
-      <p className="col-span-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-        recusandae.
-      </p>
-      <CategoriesList />
-    </div>
-  );
+  return <Articles />;
 }
