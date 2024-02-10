08a311e80369bebd0b27cef9af344a11afe9a692
Heisenberg <omaradelawad20013@gmail.com>
2024-02-10 05:53:20 +0200

    updated the categories api to send the number of articles too

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,5 +1,6 @@
 import { connectToDB } from "@/db/index";
 import Category from "@/models/Category";
+import Article from "@/models/Article";
 
 export const GET = async (request, { params }) => {
   try {
@@ -7,7 +8,22 @@ export const GET = async (request, { params }) => {
 
     const categories = await Category.find({});
 
-    return new Response(JSON.stringify(categories), { status: 200 });
+    // Fetch the number of articles for each category
+    const categoriesWithArticleCount = await Promise.all(
+      categories.map(async (category) => {
+        const totalArticles = await Article.countDocuments({
+          category: category._id,
+        });
+        return {
+          ...category.toObject(),
+          totalArticles,
+        };
+      })
+    );
+
+    return new Response(JSON.stringify(categoriesWithArticleCount), {
+      status: 200,
+    });
   } catch (error) {
     console.log(error);
     return new Response("Failed to fetch all prompts", { status: 500 });
