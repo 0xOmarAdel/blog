dd9d1b0a495ab54bf7d482be51ba1bb44e8acbf2
Heisenberg <omaradelawad20013@gmail.com>
2024-02-10 05:45:13 +0200

    updated categories backend route

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,26 +1,15 @@
-import mysql from "mysql";
-import { fetchCategoriesQuery } from "@/db/index";
+import { connectToDB } from "@/db/index";
+import Category from "@/models/Category";
 
 export const GET = async (request, { params }) => {
   try {
-    var connection = mysql.createConnection(process.env.MYSQL_URI);
+    await connectToDB();
 
-    connection.connect();
+    const categories = await Category.find({});
 
-    const queryResult = await new Promise((resolve, reject) => {
-      connection.query(fetchCategoriesQuery, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-
-    connection.end();
-
-    return new Response(JSON.stringify(queryResult), { status: 200 });
+    return new Response(JSON.stringify(categories), { status: 200 });
   } catch (error) {
-    return new Response("Internal Server Error", { status: 500 });
+    console.log(error);
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
