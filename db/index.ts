2dd7c74f4c74d45eca868274f76094dbc5d03146
Heisenberg <omaradelawad20013@gmail.com>
2024-02-10 05:31:38 +0200

    connect to db function

--- db/index.ts
+++ db/index.ts
@@ -1,3 +1,24 @@
-export const fetchArticlesQuery = "SELECT * FROM article";
+import mongoose from "mongoose";
 
-export const fetchCategoriesQuery = "SELECT * FROM category";
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      dbName: "blog",
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
