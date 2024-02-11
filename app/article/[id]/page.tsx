d3d299eb0f0801cd5b5936eb8fd3ef264a209b30
Heisenberg <omaradelawad20013@gmail.com>
2024-02-11 12:26:08 +0200

    added the article comments to the article page

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,8 +1,14 @@
-import { getArticle } from "@/db/articleActions";
+import Comment from "@/components/Comment";
+import Divider from "@/components/ui/Divider";
+import { getArticle, getArticleComments } from "@/db/articleActions";
 import Image from "next/image";
+import { CommentType } from "@/types/CommentType";
 
 const page = async ({ params }) => {
   const article = await getArticle(params.id);
+  const articleComments = (await getArticleComments(
+    params.id
+  )) as CommentType[];
 
   return (
     <div className="flex flex-col gap-5">
@@ -11,6 +17,12 @@ const page = async ({ params }) => {
       </div>
       <h2 className="text-3xl text-gray-700 font-semibold">{article.title}</h2>
       <p className="text-lg">{article.description}</p>
+      <Divider />
+      <div className="flex flex-col gap-6">
+        {articleComments.map((comment: CommentType) => (
+          <Comment key={comment._id} comment={comment} />
+        ))}
+      </div>
     </div>
   );
 };
