682639b57653b038a17c91a84fc69177c2fd75de
Heisenberg <omaradelawad20013@gmail.com>
2024-02-12 10:44:16 +0200

    card props type

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,4 +1,8 @@
-const Card = ({ children }) => {
+interface CardProps {
+  children: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ children }) => {
   return (
     <div className="relative h-fit w-full bg-white shadow-md rounded-md overflow-hidden">
       {children}
